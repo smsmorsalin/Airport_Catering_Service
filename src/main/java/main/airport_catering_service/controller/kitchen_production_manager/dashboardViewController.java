5b74458f567554277c16087352f9333b14803e60
Saifuzzaman Kaif <machining1990@gmail.com>
2026-07-24 22:21:31 +0600

    do uI design

---
+++ src/main/java/main/airport_catering_service/controller/kitchen_production_manager/dashboardViewController.java
@@ -0,0 +1,7 @@
+package main.airport_catering_service.controller.kitchen_production_manager;
+
+public class dashboardViewController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
