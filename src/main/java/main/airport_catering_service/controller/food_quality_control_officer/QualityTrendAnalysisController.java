e92de8f9d57baab69f31b1d955656298725aab67
Saifuzzaman Kaif <machining1990@gmail.com>
2026-07-26 00:23:45 +0600

    create controller class of food quality control officer

---
+++ src/main/java/main/airport_catering_service/controller/food_quality_control_officer/QualityTrendAnalysisController.java
@@ -0,0 +1,67 @@
+package main.airport_catering_service.controller.food_quality_control_officer;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+
+public class QualityTrendAnalysisController
+{
+    @javafx.fxml.FXML
+    private Label fxidDisplayCommonDefectLabel;
+    @javafx.fxml.FXML
+    private Label fxidDisplayComplianceRateLabel;
+    @javafx.fxml.FXML
+    private DatePicker endDatePicker;
+    @javafx.fxml.FXML
+    private Label fxidDisplayGenerateQAReportLabel;
+    @javafx.fxml.FXML
+    private DatePicker startDatePicker;
+    @javafx.fxml.FXML
+    private Label fxidDisplayRejectedBatchesLabel;
+    @javafx.fxml.FXML
+    private Label fxidDisplayPassedBatchesLabel;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void clearOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void sideBarRejectBatchOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void sideBarFoodSafetyCheckOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void sideBarReceiveInspectionRequestOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void analyzeTrendsOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void sideBarPerformInspectionOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void homeButtonAirportCateringServiceOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void sidebarManageQualityIssueOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void sideBarGenerateQAReportOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void sideBarApproveBatchOnAction(ActionEvent actionEvent) {
+    }
+}
