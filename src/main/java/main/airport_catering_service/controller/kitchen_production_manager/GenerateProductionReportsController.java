a61518b1706bab757165abd5888295878ab4c708
Saifuzzaman Kaif <machining1990@gmail.com>
2026-07-25 22:42:26 +0600

    create all controller class on kitchen production manager user 9

---
+++ src/main/java/main/airport_catering_service/controller/kitchen_production_manager/GenerateProductionReportsController.java
@@ -0,0 +1,74 @@
+package main.airport_catering_service.controller.kitchen_production_manager;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+
+public class GenerateProductionReportsController
+{
+    @javafx.fxml.FXML
+    private DatePicker endDatePicker;
+    @javafx.fxml.FXML
+    private Label fxidDisplayTotalProductionOrdersLabel;
+    @javafx.fxml.FXML
+    private DatePicker startDatePicker;
+    @javafx.fxml.FXML
+    private ComboBox reportTypeComboBox;
+    @javafx.fxml.FXML
+    private Label fxidDisplayTotalMealsProducedLabel;
+    @javafx.fxml.FXML
+    private Label fxidDisplayDelayedProductionsLabel;
+    @javafx.fxml.FXML
+    private Label fxidDisplayAverageProductionTimeLabel;
+    @javafx.fxml.FXML
+    private Label fxidDisplayCompletionRateLabel;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void sideBarmanageMealPreparationOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void clearOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void sideBarHandleRejectedBatchOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void sidebarAirportCateringServiceButtonOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void sideBarRequestQualityInspectionOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void sideBarUpdateProductionStatusOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void sideBarMonitorCookingProgressOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void sideBarViewProductionTasksOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void sideBarassignKitchenStaffOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void confirmOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void eportPDFOnAction(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/main/airport_catering_service/controller/kitchen_production_manager/ReceiveApprovedCateringOrdersController.java
@@ -0,0 +1,100 @@
+package main.airport_catering_service.controller.kitchen_production_manager;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
+
+public class ReceiveApprovedCateringOrdersController
+{
+    @javafx.fxml.FXML
+    private TableColumn productionOrderIDTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn mealTypesTableView;
+    @javafx.fxml.FXML
+    private TableColumn airlineNameTableView;
+    @javafx.fxml.FXML
+    private TableColumn deliveryDeadlineTableView;
+    @javafx.fxml.FXML
+    private TableView mainTableView;
+    @FXML
+    private TextField productionTaskIDTextField;
+    @FXML
+    private TableColumn mealQuantityTableView;
+    @FXML
+    private TextField orderIDTextField1;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void clearOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void searchOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void confirmOnAction(ActionEvent actionEvent) {
+    }
+
+    @Deprecated
+    public void sidebarAirporCateringServiceButtonOnClick(ActionEvent actionEvent) throws IOException {
+//        FXMLLoader fxmlL =  new FXMLLoader(dashboardViewController.class.getResource("dashboardView"));
+//        Node node = fxmlL.load();
+//        mainPane.getChildren().setAll(node);
+    }
+
+    @Deprecated
+    public void manageMealPreparationOnAction(ActionEvent actionEvent) {
+    }
+
+    @Deprecated
+    public void assignKitchenStaffOnAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void sideBarmanageMealPreparationOnAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void sideBarHandleRejectedBatchOnAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void sidebarAirportCateringServiceButtonOnClick(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void sideBarRequestQualityInspectionOnAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void sideBarUpdateProductionStatusOnAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void sideBarMonitorCookingProgressOnAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void sideBarassignKitchenStaffOnAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void homeButtonAirportCateringServiceOnAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void sideBarKitchenPerformanceReportOnAction(ActionEvent actionEvent) {
+    }
+}
