5cfae33a30df7d79988f75b3ca704faf8d0c75dd
ooronnoromy <cseromycse@gmail.com>
2026-07-28 01:47:52 +0600

    Create Package Controller for customer_support_officer

---
+++ src/main/java/main/airport_catering_service/controller/customer_support_officer/GenerateReportsViewController.java
@@ -0,0 +1,29 @@
+package main.airport_catering_service.controller.customer_support_officer;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class GenerateReportsViewController
+{
+    @javafx.fxml.FXML
+    private Button generateReportBtn;
+    @javafx.fxml.FXML
+    private TextField airlineField;
+    @javafx.fxml.FXML
+    private Label reportStatusLabel;
+    @javafx.fxml.FXML
+    private ComboBox reportTypeCombo;
+    @javafx.fxml.FXML
+    private ComboBox statusCombo;
+    @javafx.fxml.FXML
+    private ComboBox dateRangeCombo;
+    @javafx.fxml.FXML
+    private Button exportPdfBtn;
+    @javafx.fxml.FXML
+    private Label generatedReportLabel;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
---
+++ src/main/java/main/airport_catering_service/controller/customer_support_officer/TrackComplaintViewController.java
@@ -0,0 +1,31 @@
+package main.airport_catering_service.controller.customer_support_officer;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextField;
+
+public class TrackComplaintViewController
+{
+    @javafx.fxml.FXML
+    private Label complaintIdLabel;
+    @javafx.fxml.FXML
+    private Button searchButton;
+    @javafx.fxml.FXML
+    private Label orderLabel;
+    @javafx.fxml.FXML
+    private ProgressBar progressBar;
+    @javafx.fxml.FXML
+    private Label categoryLabel;
+    @javafx.fxml.FXML
+    private TextField complaintIdField;
+    @javafx.fxml.FXML
+    private Label airlineLabel;
+    @javafx.fxml.FXML
+    private Label statusLabel;
+    @javafx.fxml.FXML
+    private Label departmentLabel;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
