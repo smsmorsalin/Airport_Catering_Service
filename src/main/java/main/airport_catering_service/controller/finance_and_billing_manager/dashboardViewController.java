4e77a644a00bdadabbaa2f0afec7139678c88ab0
ooronnoromy <cseromycse@gmail.com>
2026-07-28 15:14:05 +0600

    Create Action Event for FinanceAndBillingManager

--- src/main/java/main/airport_catering_service/controller/finance_and_billing_manager/dashboardViewController.java
+++ src/main/java/main/airport_catering_service/controller/finance_and_billing_manager/dashboardViewController.java
@@ -1,6 +1,10 @@
 package main.airport_catering_service.controller.finance_and_billing_manager;
 
 import javafx.event.ActionEvent;
+import user.FinanceAndBillingManager;
+
+import javax.imageio.IIOException;
+import java.io.IOException;
 
 public class dashboardViewController
 {
@@ -14,5 +18,10 @@ public class dashboardViewController
 
     @javafx.fxml.FXML
     public void sidebarHomePageOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void sideBarCalculateCostButtonOnAction(ActionEvent actionEvent) throws IOException {
+        FinanceAndBillingManager.viewCalculateCost(actionEvent);
     }
 }
--- src/main/java/user/FinanceAndBillingManager.java
+++ src/main/java/user/FinanceAndBillingManager.java
@@ -1,4 +1,39 @@
 package user;
 
-public class FinanceAndBillingManager {
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+public class FinanceAndBillingManager extends Employee {
+
+    public FinanceAndBillingManager(int userId, String password, String fullName, String dateOfBirth, String gender, String email, String phone, String address, String status, int employeeId, LocalDate joinDate, String department, String designation, String shift, float salary) {
+        super(userId, password, fullName, dateOfBirth, gender, email, phone, address, status, employeeId, joinDate, department, designation, shift, salary);
+    }
+
+    @Override
+    public void viewDashboard() throws IOException{
+
+    }
+
+    @Override
+    public boolean updateProfile(){
+        return false;
+    }
+
+    public static void viewCalculateCost(javafx.event.ActionEvent event) throws IOException{
+        FXMLLoader loader = new FXMLLoader(FinanceAndBillingManager.class.getResource("/FinanceAndBillingManager/CalculateCostView.fxml"));
+        Parent root = loader.load();
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
