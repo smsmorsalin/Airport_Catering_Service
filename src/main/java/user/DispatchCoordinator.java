0672281c74e89a16f875f03a41eb0bc319287345
Sihab2006 <sihabhasan112006@gmail.com>
2026-07-30 20:05:16 +0600

    Merge remote-tracking branch 'origin/sihab' into sihab

--- src/main/java/user/DispatchCoordinator.java
+++ src/main/java/user/DispatchCoordinator.java
@@ -1,4 +1,10 @@
 package user;
 
-public class DispatchCoordinator {
+import java.time.LocalDate;
+
+public class DispatchCoordinator extends Employee{
+
+    public DispatchCoordinator(int userId, String password, String fullName, String dateOfBirth, String gender, String email, String phone, String address, String status, int employeeId, LocalDate joinDate, String department, String designation, String shift, float salary) {
+        super(userId, password, fullName, dateOfBirth, gender, email, phone, address, status, employeeId, joinDate, department, designation, shift, salary);
+    }
 }
