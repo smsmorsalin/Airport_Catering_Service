d033d5af6ddf1c0ba51922ca824633274cfa8c31
ooronnoromy <cseromycse@gmail.com>
2026-08-03 23:14:15 +0600

    Controller Update

--- src/main/java/main/airport_catering_service/controller/customer_support_officer/dashboardViewController.java
+++ src/main/java/main/airport_catering_service/controller/customer_support_officer/dashboardViewController.java
@@ -1,6 +1,7 @@
 package main.airport_catering_service.controller.customer_support_officer;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.*;
 import user.CustomerSupportOfficer;
 
 import java.io.IOException;
@@ -8,8 +9,27 @@ import java.io.IOException;
 public class dashboardViewController
 {
     @javafx.fxml.FXML
+    private TableColumn descriptionTableView;
+    @javafx.fxml.FXML
+    private Label emargencyIssueLabel;
+    @javafx.fxml.FXML
+    private TableColumn ticketIDTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn statusTableView;
+    @javafx.fxml.FXML
+    private ComboBox statusComboBox;
+    @javafx.fxml.FXML
+    private TextField enterticketIDTextField;
+    @javafx.fxml.FXML
+    private TableView mainTableView;
+    @javafx.fxml.FXML
+    private TableColumn issueTypeTableView;
+
+    @javafx.fxml.FXML
     public void initialize() {
         CustomerSupportOfficer c = null;
+
+
 
     }
 
@@ -63,5 +83,9 @@ public class dashboardViewController
     @javafx.fxml.FXML
     public void sideBarFlightDelayRequestButtonOnAction(ActionEvent actionEvent) throws IOException {
         CustomerSupportOfficer.viewFlightDelayRequest(actionEvent);
+    }
+
+    @javafx.fxml.FXML
+    public void updateOnAction(ActionEvent actionEvent) {
     }
 }
